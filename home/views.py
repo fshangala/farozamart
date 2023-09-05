cff1cb29b72bcf1520ff6c047a13ca4b37a49ecf
fshangala <fshangala@gmail.com>
2023-09-06 01:36:56 +0200

    configured messages

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import View
 from store.models import Purchase
+from django.contrib import messages
 
 # Create your views here.
 home_context={
@@ -12,4 +13,5 @@ class Home(View):
     context=home_context
     latest_listings = Purchase.objects.all()[:8]
     context['latest_listings'] = latest_listings
+    messages.info(request,'Welcome to farozamart')
     return render(request,self.template_name,context)
