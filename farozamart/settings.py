f3f721dbfb724a28eeb3026ad81fce608562072c
fshangala <fshangala@gmail.com>
2023-11-25 09:17:09 +0200

    message tags

--- farozamart/settings.py
+++ farozamart/settings.py
@@ -11,6 +11,15 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.SUCCESS: "alert-success",
+    messages.INFO: "alert-info",
+    messages.WARNING: "alert-warning",
+    messages.ERROR: "alert-danger",
+    50: "alert-danger",
+}
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
