b146c61905c774e9871f930ff51d33583d3fa3e0
fshangala <fshangala@gmail.com>
2023-12-12 08:10:01 +0200

    managing static files

--- farozamart/settings.py
+++ farozamart/settings.py
@@ -150,7 +150,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_ROOT=BASE_DIR / 'media'
 MEDIA_URL='/media/'
